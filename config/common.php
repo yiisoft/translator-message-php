62a440bdd2821f3b8d52c1943a71ae870ea76dfc
yiiliveext <yiiliveext@gmail.com>
2021-05-09 16:41:24 +0300

    Disallow definition objects in configs, except ReferenceInterface

--- config/common.php
+++ config/common.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 use Yiisoft\Translator\MessageReaderInterface;
 use Yiisoft\Translator\Message\Php\MessageSource;
 use Yiisoft\Aliases\Aliases;
+use Yiisoft\Factory\Definition\DynamicReference;
 
 return [
     MessageReaderInterface::class => [
         'class' => MessageSource::class,
         '__construct()' => [
-            fn (Aliases $aliases) => $aliases->get('@message'),
+            DynamicReference::to(fn (Aliases $aliases) => $aliases->get('@message')),
         ],
     ],
 ];
